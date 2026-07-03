754ecedd15c00e4d55f0111226057a254107b1af
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-07-03 16:04:19 +0530

    feat: add reusable container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Container from "./components/Container"
+
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-600">
-      Tailwind is working!
-    </h1>
+    <Container>
+      <h1 className="text-4xl font-bold">Portfolio</h1>
+      
+    </Container>
   )
 }
 
